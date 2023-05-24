1d0b5a4a03f28fda835a61d08ef671dd407ac757
KartheeshReddy <65816735+KartheeshReddy@users.noreply.github.com>
2023-05-24 21:46:01 +0530

    Update 2542. Maximum Subsequence Score.cpp

--- 2542. Maximum Subsequence Score.cpp
+++ 2542. Maximum Subsequence Score.cpp
@@ -62,3 +62,56 @@ public:
         return solve(memo,nums1,n-1,nums2,INT_MAX,0,k);
     }
 };
+
+
+// approach 2 -- beautiful (https://leetcode.com/problems/maximum-subsequence-score/discuss/3083376/Easy-or-C%2B%2B-or-With-intuition-and-detailed-explanation-for-beginners)
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+class Solution {
+public:
+    long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n=nums1.size();
+        
+        long long res=0;
+
+        priority_queue<pair<int,int>> maxHeap;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> minHeap;    
+    
+        for(int i=0;i<n;i++)
+            maxHeap.push(make_pair(nums1[i],i));
+    
+        long long summation=0;
+        while(!maxHeap.empty() and k>0)
+        {
+            summation+=maxHeap.top().first;
+            minHeap.push(make_pair(nums2[maxHeap.top().second],maxHeap.top().second));
+            maxHeap.pop();
+            k--;
+        }
+
+        long long minElement=minHeap.top().first;
+
+        res=max(res,summation*minElement);
+        while(!maxHeap.empty())
+        {
+            summation-=nums1[minHeap.top().second];
+            minHeap.pop();
+            summation+=maxHeap.top().first;
+            minHeap.push(make_pair(nums2[maxHeap.top().second],maxHeap.top().second));
+            maxHeap.pop();
+            minElement=minHeap.top().first;
+            res=max(res,summation*minElement);
+        }
+
+        return res;
+    }
+};
+
+
+// approach 3 -- similar to above but instead of maxheap for nums1 vector can used by sorting in descending order
