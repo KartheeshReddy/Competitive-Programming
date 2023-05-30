20c81c3749b87f6d4346157057f08ae52d141cf6
KartheeshReddy <65816735+KartheeshReddy@users.noreply.github.com>
2023-05-30 09:41:31 +0530

    Update 2028. Find Missing Observations.cpp

--- 2028. Find Missing Observations.cpp
+++ 2028. Find Missing Observations.cpp
@@ -40,4 +40,43 @@ public:
     }
 };
 
+// approach 2
 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+public:
+    
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        int m=rolls.size();
+
+        int totSum=mean*(m+n);
+        int sumOfMElements=accumulate(rolls.begin(),rolls.end(),0);        
+        // self understandable 
+        int targetSum=totSum-sumOfMElements;
+
+        // if the n elements are all ,1s then sum would be n..,6s then sum would be 6*n if the target sum is not in this range then return nothing
+        if(targetSum<n or targetSum>6*n)
+            return {};
+
+        // initialize n-sized array with 1s
+        vector<int> arr(n,1);
+        // reduce the target by n as n 1s have been considered
+        targetSum-=n;
+
+        for(int i=0;i<n;i++)
+        {
+            // the idea is to fill the array with 6s as much as possible and remaining values will be 1s
+            while(arr[i]<6 and targetSum>0)
+            {
+                arr[i]++;
+                targetSum--;
+            }
+        }
+
+        return arr;
+    }
+};
