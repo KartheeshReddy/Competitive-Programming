c4238a84e462e4d5a78abe73aace7047f774d7f9
KartheeshReddy <65816735+KartheeshReddy@users.noreply.github.com>
2023-06-07 09:49:56 +0530

    Create 1318. Minimum Flips to Make a OR b Equal to c.cpp

---
+++ 1318. Minimum Flips to Make a OR b Equal to c.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int noOfBitsFlipped=0;
+
+        while(a!=0 or b!=0 or c!=0)
+        {
+            int bitA=((a&1)==0?0:1);
+            a>>=1;
+            int bitB=((b&1)==0?0:1);
+            b>>=1;
+            int bitC=((c&1)==0?0:1);
+            c>>=1;
+            
+            // cout<<bitA<<" "<<bitB<<" "<<bitC<<endl;
+
+            if(bitC==0)
+            {
+                if(bitA==0 and bitB==0)
+                    continue;
+                // if any of the bit is 1 then it must be flipped to 0
+                if(bitA==1) noOfBitsFlipped++;
+                if(bitB==1) noOfBitsFlipped++;
+            }
+            else
+            {
+                if(bitA==1 or bitB==1)
+                    continue;
+                // it says that both the bits are 0 hence flip any of them to 1
+                noOfBitsFlipped++;
+            }
+        }      
+
+        return noOfBitsFlipped;
+    }
+};
