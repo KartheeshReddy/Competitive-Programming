35d49e703cbef1d1e4f4aa5b2cf5e7dc5616d668
KartheeshReddy <65816735+KartheeshReddy@users.noreply.github.com>
2023-06-26 15:12:26 +0530

    Update 2750. Ways to Split Array Into Good Subarrays.cpp

--- 2750. Ways to Split Array Into Good Subarrays.cpp
+++ 2750. Ways to Split Array Into Good Subarrays.cpp
@@ -36,3 +36,84 @@ public:
         return solve(nums,n,0,0);
     }
 };
+
+
+// linear time
+
+// this logic comes purely from visualization 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+public:
+
+    int numberOfGoodSubarraySplits(vector<int>& nums) {
+        int n=nums.size(),res=0;
+
+        long long noOfWaysToSplitAtPrev1=-1;
+        long long countZeros;
+
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==0)
+                countZeros++;
+            else
+            {
+                // for the first 1 there will be only 1 split
+                if(noOfWaysToSplitAtPrev1==-1)
+                    noOfWaysToSplitAtPrev1=1;
+                else
+                {
+                    // [1,0,0,0,1]
+                    // [1][0,0,0,1]
+                    // [1,0][0,0,1]
+                    // [1,0,0][0,1]
+                    // [1,0,0,0][1]
+                    // the number of ways to split at the current 1 depends on the number of ways it can be splitted at previous 1 and zeros between the 1s
+
+                    noOfWaysToSplitAtPrev1=(long long)(noOfWaysToSplitAtPrev1*(countZeros+1))%1000000007;
+                }
+                countZeros=0;
+            }
+        }
+
+        return noOfWaysToSplitAtPrev1!=-1?noOfWaysToSplitAtPrev1%1000000007:0;
+    }
+};
+
+
+// take a look on how the count changes with each number added to the array
+
+[0]
+[0]
+---------------
+[0,1]
+[0,1]
+---------------
+[0,1,0]
+[0,1,0]
+-----------------
+[0,1,0,0]
+[0,1,0,0]
+------------------
+[0,1,0,0,1]
+[0,1] [0,0,1]
+[0,1,0] [0,1]
+[0,1,0,0] [1]
+--------------------
+[0,1,0,0,1,0]
+[0,1] [0,0,1,0]
+[0,1,0] [0,1,0]
+[0,1,0,0] [1,0]
+---------------------
+[0,1,0,0,1,0,1]
+[0,1] [0,0,1] [0,1]
+[0,1] [0,0,1,0] [1]
+
+[0,1,0] [0,1] [0,1]
+[0,1,0] [0,1,0] [1]
+
+[0,1,0,0] [1] [0,1]
+[0,1,0,0] [1,0] [1]
